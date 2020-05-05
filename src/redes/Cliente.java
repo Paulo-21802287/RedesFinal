9b357c2a4b2ae3a24e9a3e0da5298b3822845511
Ricardo <grtedszxRwe TWERFG>
2020-05-05 11:43:11 +0100

    Initial commit

---
+++ src/redes/Cliente.java
@@ -0,0 +1,8 @@
+package redes;
+
+public class Cliente {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/redes/Servidor.java
@@ -0,0 +1,8 @@
+package redes;
+
+public class Servidor {
+
+    public static void main(String[] args) {
+	// write your code hereaaaaaa
+    }
+}
